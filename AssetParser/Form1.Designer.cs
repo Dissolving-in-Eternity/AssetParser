d7398c2ee062a0a44fdb98ee7d9526e9e98829b2
Prisoner Sense <prisonersense@yahoo.com>
2018-08-10 17:58:17 +0300

    Add implementation.

--- AssetParser/Form1.Designer.cs
+++ AssetParser/Form1.Designer.cs
@@ -28,13 +28,94 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.btnGetData = new System.Windows.Forms.Button();
+			this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+			this.dataGrid = new System.Windows.Forms.DataGridView();
+			this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ColTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ColBid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.ColAsk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnGetData
+			// 
+			this.btnGetData.Location = new System.Drawing.Point(12, 12);
+			this.btnGetData.Name = "btnGetData";
+			this.btnGetData.Size = new System.Drawing.Size(146, 23);
+			this.btnGetData.TabIndex = 0;
+			this.btnGetData.Text = "Get data";
+			this.btnGetData.UseVisualStyleBackColor = true;
+			this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+			// 
+			// backgroundWorker
+			// 
+			this.backgroundWorker.WorkerReportsProgress = true;
+			this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+			this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+			// 
+			// dataGrid
+			// 
+			this.dataGrid.AllowUserToAddRows = false;
+			this.dataGrid.AllowUserToDeleteRows = false;
+			this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColName,
+            this.ColTime,
+            this.ColBid,
+            this.ColAsk});
+			this.dataGrid.Location = new System.Drawing.Point(12, 41);
+			this.dataGrid.Name = "dataGrid";
+			this.dataGrid.Size = new System.Drawing.Size(544, 397);
+			this.dataGrid.TabIndex = 1;
+			// 
+			// ColName
+			// 
+			this.ColName.HeaderText = "Asset Name";
+			this.ColName.Name = "ColName";
+			// 
+			// ColTime
+			// 
+			this.ColTime.HeaderText = "Time";
+			this.ColTime.Name = "ColTime";
+			this.ColTime.Width = 200;
+			// 
+			// ColBid
+			// 
+			this.ColBid.HeaderText = "Bid";
+			this.ColBid.Name = "ColBid";
+			// 
+			// ColAsk
+			// 
+			this.ColAsk.HeaderText = "Ask";
+			this.ColAsk.Name = "ColAsk";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(568, 450);
+			this.Controls.Add(this.dataGrid);
+			this.Controls.Add(this.btnGetData);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button btnGetData;
+		private System.ComponentModel.BackgroundWorker backgroundWorker;
+		private System.Windows.Forms.DataGridView dataGrid;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ColTime;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ColBid;
+		private System.Windows.Forms.DataGridViewTextBoxColumn ColAsk;
 	}
 }
 
--- AssetParser/Form1.cs
+++ AssetParser/Form1.cs
@@ -1,20 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace AssetParser
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        private readonly string BTN_NAME_START = "Get data!";
+        private readonly string BTN_NAME_STOP = "MAKE IT STOP";
+
+        // A flag to stop reading network stream
+        bool makeItStop = true;
+        // Dictionary to store row number of known assets
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnGetData.Text = BTN_NAME_START;
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            // If we're currently reading the stream => stop
+            if (!makeItStop)
+            {
+                makeItStop = true;
+                btnGetData.Text = BTN_NAME_START;
+            }
+            // Else => reset the flag and start reading the stream
+            else
+            {
+                makeItStop = false;
+                backgroundWorker.RunWorkerAsync();
+                btnGetData.Text = BTN_NAME_STOP;
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            using (var tcpClient = new TcpClient("79.125.80.209", 4092))
+            using (var netStream = tcpClient.GetStream())
+            using (var sr = new StreamReader(netStream))
+            {
+                bool firstLine = true;
+                while (!makeItStop)
+                {
+                    // Read one line from the stream
+                    string line = sr.ReadLine();
+                    string[] tokens = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Remove Login, Password and Access granted parts from the first line
+                    // That should be first 4 tokens
+                    if (firstLine)
+                    {
+                        tokens = tokens.Skip(4).ToArray();
+                        firstLine = false;
+                    }
+
+                    // Report new asset received
+                    backgroundWorker.ReportProgress(0, tokens);
+                }
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string[] tokens = e.UserState as string[];
+
+            // Tokens should contain: Name, Time, Bid and Ask in that order
+            if(tokens?.Length == 4)
+            {
+                // If it's a known asset => get its row number
+                if(dic.ContainsKey(tokens[0]))
+                {
+                    UpdateAsset(index:dic[tokens[0]], time:tokens[1], bid:tokens[2], ask:tokens[3]);
+                }
+                // Otherwise add new asset and save it to the dictionary
+                else
+                {
+                    int index = AddNewAsset(name: tokens[0], time: tokens[1], bid: tokens[2], ask: tokens[3]);
+                    dic.Add(tokens[0], index);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adds new asset to the DataGrid
+        /// </summary>
+        /// <returns>Index of the created asset in the DataGrid</returns>
+        private int AddNewAsset(string name, string time, string bid, string ask) 
+            => dataGrid.Rows.Add(name, time, bid, ask);
+
+        /// <summary>
+        /// Updates time, bid and ask cells of a specified row
+        /// </summary>
+        private void UpdateAsset(int index, string time, string bid, string ask)
+        {
+            if (index < dataGrid.Rows.Count)
+            {
+                dataGrid.Rows[index].Cells["ColTime"].Value = time;
+                dataGrid.Rows[index].Cells["ColBid"].Value = bid;
+                dataGrid.Rows[index].Cells["ColAsk"].Value = ask;
+            }
+        }
+    }
 }
